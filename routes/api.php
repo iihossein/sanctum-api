5e7cf7d860e2aef78aa4e72db8903680e9480b6b
iihossein <hosseinnazarigit@gmail.com>
2024-02-03 12:44:15 +0330

    before adding sanctum

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/products',[ProductController::class,'index']);
+// Route::post('/products',[ProductController::class,'store']);
+// Route::get('/products/{id}',[ProductController::class,'show']);
+
+Route::resource('products',ProductController::class);
+Route::get('/products/search/{name}',[ProductController::class,'search'])->name('product.search');
+
+
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
